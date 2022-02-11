0f82beb2b9cd29d416fa1c89f8bbd1a9af748e2e
burriana <oballesterfe@uoc.edu>
2022-02-11 13:03:02 +0100

    inicion js

---
+++ javascript/inicio-js.js/main.js
@@ -0,0 +1,40 @@
+
+// const d = new Date();
+// document.getElementById("demo").innerHTML = d;
+// console.log("Estoy en la consola")
+// window.alert("Hola ultimo")
+
+
+// var personaje = 'Sonic';
+// function funcionQueDefineUnScopeLocal() {
+// var nemesis = "Robotnik";
+// console.log(" dentro de la funcion "+personaje); // Sonic
+// console.log(" dentro de la funcion "+nemesis); // Robotnik
+// }
+// console.log("fuera de la funcion "+personaje); // Sonic
+// console.log("fuera de la funcion "+nemesis); // undefined
+
+var a =121;
+var b =16;
+
+// console.log("Suma = "+(a+=b));
+// console.log("Resta = "+(a-=b));
+// console.log("Multiplicar = "+(a*=b));
+// console.log("Dividir= "+(a/=b));
+// console.log("Resta = "+(a%=b));
+
+
+var c=7;
+var d=8;
+var e=7;
+
+
+console.log(c +" es igual a "+ d +"?:  "+ (c==d));
+// console.log( c + " = " d + c==d);
+// console.log(c===d);
+// console.log(c!=d);
+// console.log(c!==d);
+// console.log(c>d);
+// console.log(c<d);
+// console.log(c>=d);
+// console.log(c<=d);
