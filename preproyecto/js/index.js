922c9e7b7ca6ff49b77c64daa515600f877b9002
burriana <97947221+burriana@users.noreply.github.com>
2022-03-15 20:49:16 +0100

    CReado carrito sin repeticion

--- preproyecto/js/index.js
+++ preproyecto/js/index.js
@@ -4,10 +4,49 @@ function buy(id) {
   cart.push(products.find((ele) => ele.id === id));
 }
 
+
+//PROTOTYPE:  cartSinRepeticion(cart)
+//DESCRIPTION: A partir de un array con los productos de compra, crea otro array donde solo aparece una vez cada producto con la cantidad comprada. Calcula el total por procusto y el total del carrito.
+
+function cartSinRepeticion(cart){
+  const cartSinRepes = [];
+  const ids =[];
+  
+  cart.forEach(producto =>{
+    if(ids.includes(producto.id) === false){
+      ids.push(producto.id)
+      producto['cantidad'] = 1
+      cartSinRepes.push(producto)
+    }else{
+      producto.cantidad += 1
+    }
+  
+  })
+ // Calculo del precio total por producto
+  cartSinRepes.forEach(linea =>{
+    linea['total'] = linea.price * linea.cantidad
+  })
+
+  return cartSinRepes
+}
+  // Calculo del total del carrito
+  function totalCarrito(cartSinRepes){
+  let totalCarrito= cartSinRepes.reduce((suma,linea)=> suma += linea.total,0).toFixed(3)
+  return totalCarrito
+  
+ 
+}
+
+
+
+
+
+
 //PROTOTYPE: showArrayAsTable(array), array is an Array
 //DESCRIPTION: Show in the console de array as a table.
 function showArrayAsTable(array) {
   console.table(array);
+
 }
 
 //PROTOTYPE: cleanArray(array) array is an Array
@@ -47,7 +86,7 @@ function calculateTotal() {
 function computeTotal(array) {
   let total = 0;
   array.forEach((ele) => {
-    console.log(`Valor de ${ele.name} es: ${ele.price}`);
+    //console.log(`Valor de ${ele.name} es: ${ele.price}`);
     total += ele.price;
   });
   console.log(`--- Total de la compra por Producto: ${total.toFixed(3)}€ ---`);
@@ -55,7 +94,6 @@ function computeTotal(array) {
 
 
 
-//reto
 function crearObject(array) {
 
   const model = {
