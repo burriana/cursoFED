fcf707c63e2975bdcd7ebab492a61058abecb5c2
burriana <97947221+burriana@users.noreply.github.com>
2022-04-27 06:46:17 +0200

    funciona

---
+++ vuecli2/crud-2/vue-js-client-crud/src/http-common.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+export default axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
+
+// Remember to change the baseURL, it depends on REST APIs url that your Server configures.
